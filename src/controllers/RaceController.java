7966d929d26afbde20c3d80888112e684849286c
Pierre VILLIERS <pierre.villiers@ynov.com>
2022-03-04 16:46:38 +0100

    Creation methode printAllRace dans RaceController avec tests

--- src/controllers/RaceController.java
+++ src/controllers/RaceController.java
@@ -1,5 +1,12 @@
 package controllers;
 
+import dao.RaceDAO;
+
 public class RaceController {
 
+	RaceDAO rDao = new RaceDAO();
+	
+	public String printAllRaces() {
+		return "echec";
+	}
 }
--- tests/controllersTests/RaceControllerTests.java
+++ tests/controllersTests/RaceControllerTests.java
@@ -1,5 +1,20 @@
 package controllersTests;
 
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import controllers.RaceController;
+import models.Data;
+
 public class RaceControllerTests {
 
+	Data data = Data.init();
+	
+	RaceController rCrt = new RaceController();
+	
+	@Test
+	public void printAllRaceTestOk() {
+		assertTrue(rCrt.printAllRaces().equals("ok"));
+	}
 }
